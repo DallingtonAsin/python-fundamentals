b772b3d946e4bf3c8ecc4d29fad2e57dc719a9f3
DallingtonAsin <asimgwire50dallington@gmail.com>
2022-10-30 18:29:32 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is Python script for fundamentals
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# basic method
+def calculate(x, y):
+    return x + y
+
+
+print('The sum of numbers is ', calculate(34, 26))
